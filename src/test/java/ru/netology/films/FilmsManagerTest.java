47c3ed98fd7e793c216e589706f8d385c1d1fb90
Neto Andrey <diomand761988@gmail.com>
2026-03-02 21:18:11 +0300

    Fix pom.xml and finalized the code and tests

--- src/test/java/ru/netology/films/FilmsManagerTest.java
+++ src/test/java/ru/netology/films/FilmsManagerTest.java
@@ -1,4 +1,33 @@
 package ru.netology.films;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class FilmsManagerTest {
+    @Test
+    public void test1() {
+        FilmsManager manager = new FilmsManager();
+
+        manager.addFilm("Movie 1");
+        manager.addFilm("Movie 2");
+        manager.addFilm("Movie 3");
+
+        String[] expected = {"Movie 1", "Movie 2", "Movie 3"};
+        String[] actual = manager.findAll();
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void test2() {
+        FilmsManager manager = new FilmsManager();
+
+        manager.addFilm("Movie 1");
+        manager.addFilm("Movie 2");
+        manager.addFilm("Movie 3");
+
+        String[] expected = {"Movie 3", "Movie 2", "Movie 1"};
+        String[] actual = manager.findLast();
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
 }
